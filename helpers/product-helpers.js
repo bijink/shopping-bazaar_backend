57828534a061ebbc587675cc280c332397cbee78
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-03-15 20:18:02 +0530

    Integrate data from mongodb in page
    Tutorial video_part_18/project_part_4

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,13 +1,20 @@
 const db = require("../config/connection");
+var collections = require("../config/collections");
 
 module.exports = {
   addProduct: (product, callback) => {
     db.get()
-      .collection("product")
+      .collection(collections.PRODUCT_COLLECTION)
       .insertOne(product)
       .then(data => {
-        // console.log("DATA:: ", data);
+        console.log("DATA:: ", data);
         callback(data.insertedId.toString());
       });
+  },
+  getAllProducts: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray();
+      resolve(products);
+    });
   },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,13 @@
 var express = require("express");
+const productHelpers = require("../helpers/product-helpers");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let products = [
-    {
-      name: "Poco X5 Pro",
-      category: "mobile",
-      description: "Mobile designed by Xiaomi",
-      imgUrl: "https://www.91-cdn.com/hub/wp-content/uploads/2023/02/POCO-X5-Pro.jpg",
-    },
-    {
-      name: "Caspian",
-      category: "water bottle",
-      description: "Stainles steel water bottle of 1000ml",
-      imgUrl: "https://m.media-amazon.com/images/I/51tfChUERVL.jpg",
-    },
-    {
-      name: "iPhone 15 Pro",
-      category: "mobile",
-      description: "Mobile designed by Apple",
-      imgUrl:
-        "https://images.macrumors.com/t/oiWkxB5isnYn8BFbcgKsnDIUOdI=/800x0/smart/article-new/2023/09/iPhone-15-Pro-Lineup-Feature.jpg?lossy",
-    },
-    {
-      name: "Dell OptiPlex 3020",
-      category: "CPU",
-      description: "PC CPU with Intel i5 processor",
-      imgUrl:
-        "https://www.myorderstore.com/image/cache/catalog/Products/Products/delloptiplex3020pc0-550x550w.jpg.webp",
-    },
-  ];
-
-  res.render("index", { products, admin: false });
+  productHelpers.getAllProducts().then(products => {
+    // console.log("PRODUCTS:: ", products);
+    res.render("user/view-products", { products, admin: false });
+  });
 });
 
 module.exports = router;
