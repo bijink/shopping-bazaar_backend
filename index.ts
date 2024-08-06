7bfc2dcc8c6549814238bcf75f305583df9930a7
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-08-06 16:24:02 +0530

    Add morgan package

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
 import session from 'express-session';
+import logger from 'morgan';
 import connectDB from './mongoose/connect';
 import routes from './routes';
 
@@ -13,7 +14,7 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
-// app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
