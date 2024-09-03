a895511922e3076779a97696d995c49003a5cd9e
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-09-03 19:56:40 +0530

    Add route to get multiple images

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { checkSchema } from 'express-validator';
-import { promises } from 'fs';
+import fs from 'fs';
 import path from 'path';
+import { Base64Image } from '../types/global.type';
 import { authenticateJwtToken, authenticateUserRole, validateRequest } from '../utils/middlewares';
 import { uploadFile } from '../utils/multer';
 import { fileUploadSchema } from '../utils/validationSchemas';
@@ -28,13 +29,13 @@ router.post(
   uploadFile.array('files', 4),
   (request, response) => {
     if (!request.files) return response.status(400).send({ message: 'no file found on request' });
-
-    const fileNames: string[] = [];
+    const fileNames: (string | null)[] = [];
     const reqFiles = request?.files as Express.Multer.File[];
     const reqFilesLength = reqFiles?.length as number;
     if (reqFiles?.length) {
       for (let i = 0; i < reqFilesLength; i++) {
         if (reqFiles[i].filename !== 'no-image') fileNames.push(reqFiles[i].filename);
+        else fileNames.push(null);
       }
     }
     response
@@ -48,12 +49,30 @@ router.get('/get-image/:imageName', (request, response) => {
   const image = path.join(process.cwd(), 'uploads/image', imageName);
   response.status(200).sendFile(image);
 });
+// #get-multi-images route
+router.post('/get-multi-images', (request, response) => {
+  const reqImgs = request.body.images;
+  const images: (Base64Image | null | undefined)[] = reqImgs.map((imageName: string | null) => {
+    if (imageName) {
+      const imagePath = path.join(process.cwd(), 'uploads/image', imageName);
+      if (fs.existsSync(imagePath)) {
+        const fileData = fs.readFileSync(imagePath);
+        return {
+          name: imageName,
+          data: fileData.toString('base64'),
+          mimeType: imageName.split('.')[1],
+        };
+      }
+    } else return null;
+  });
+  response.status(200).json({ images });
+});
 // #delete-image route
 router.delete('/delete-image', authenticateJwtToken, (request, response) => {
   const reqFileNames = request.body as string[];
   async function deleteFiles(files: string[]) {
     try {
-      await Promise.all(files.map((file) => promises.unlink(`uploads/image/${file}`)));
+      await Promise.all(files.map((file) => fs.promises.unlink(`uploads/image/${file}`)));
       response.status(200).send({ message: 'all files deleted successfully' });
     } catch (err) {
       response.status(404).send(err);
---
+++ types/global.type.ts
@@ -0,0 +1,5 @@
+export interface Base64Image {
+  name: string;
+  data: string;
+  mimeType: string;
+}
