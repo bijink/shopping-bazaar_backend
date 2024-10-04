f3b2f6129802b6060030f9734fc459275e9253a6
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-10-04 11:20:49 +0530

    Update node.js.yml

--- mongoose/collections.ts
+++
@@ -1,9 +0,0 @@
-const collections = {
-  ADMIN_COLLECTION: 'admin',
-  PRODUCT_COLLECTION: 'products',
-  USER_COLLECTION: 'users',
-  CART_COLLECTION: 'cart',
-  ORDER_COLLECTION: 'order',
-};
-
-export default collections;
